698f570a483d183a355e33d26043941eaed1b1e3
Biggest Shark <wu920327@gmail.com>
2026-03-09 20:30:13 +0800

    navbar和標題位置互換

--- Website/resources/views/layouts/app.blade.php
+++ Website/resources/views/layouts/app.blade.php
@@ -3,20 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>@yield('title', 'welcome')</title>
     
     <link rel="stylesheet" href="{{ asset('css/welcome.blade.css') }}">
 
     @yield('custom_css')
 </head>
 <body>
-
-    
-
-    <div class="content">
-        @yield('content')
-    </div>
-
     <nav>
         <a href="{{ route('introduction') }}">自我介紹</a>
         <a href="{{ route('socialMedia') }}">社群平台</a>
@@ -24,6 +16,12 @@
         <a href="{{ route('goals') }}">未來目標</a>
         <a href="{{ route('contact') }}">聯絡方式</a>
     </nav>
+
+    <title>@yield('title', 'welcome')</title>
+
+    <div class="content">
+        @yield('content')
+    </div>
 
 </body>
 </html>
