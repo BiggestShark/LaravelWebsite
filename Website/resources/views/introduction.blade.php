286593e9d04b7e0d302eb6cf820b9f947a0a5a4b
Biggest Shark <wu920327@gmail.com>
2026-07-20 12:43:40 +0800

    自我介紹頁新增硬體資訊，內容更改

--- Website/resources/views/introduction.blade.php
+++ Website/resources/views/introduction.blade.php
@@ -24,31 +24,19 @@
 
         <section class="page-section">
             <h2>
-                遊戲
+                興趣
             </h2>
             <p>
-                我只要放假就是在打遊戲
+                平常就玩玩pc遊戲和看動畫
             </p>
             <p>
-                通常是一些單機遊戲
+                遊戲可以看看遊戲成就那邊
             </p>
             <p>
-                如果需要遊戲推薦可以dc私訊
-            </p>
-        </section>
-
-        <section class="page-section">
-            <h2>
-                動畫
-            </h2>
-            <p>
-                目前只看百合
+                動畫目前只看GL，參考下面的Anilist
             </p>
             <p>
-                包括漫畫和小說
-            </p>
-            <p>
-                下面連結可以看我有什麼書
+                還有一個書籍清單google sheet
             </p>
             <a
                 href="https://anilist.co/user/BiggestShark/animelist"
@@ -74,6 +62,94 @@
                     <span class="sheet-title">Biggest Shark的圖書館</span>
                 </div>
             </a>
+        </section>
+
+        <section class="page-section">
+            <h2>
+                電腦硬體
+            </h2>
+            <dl class="hardware-list">
+                <div class="hardware-item">
+                    <dt>
+                        CPU
+                    </dt>
+                    <dd>
+                        Intel Core i5-12600KF
+                    </dd>
+                </div>
+                <div class="hardware-item">
+                    <dt>
+                        GPU
+                    </dt>
+                    <dd>
+                        NVIDIA GeForce RTX 4060
+                    </dd>
+                </div>
+                <div class="hardware-item">
+                    <dt>
+                        記憶體
+                    </dt>
+                    <dd>
+                        Kingston 32 GB（16 GB × 2）
+                    </dd>
+                </div>
+                <div class="hardware-item">
+                    <dt>
+                        主機板
+                    </dt>
+                    <dd>
+                        ASUS TUF GAMING B760M-PLUS WIFI II
+                    </dd>
+                </div>
+                <div class="hardware-item">
+                    <dt>
+                        滑鼠
+                    </dt>
+                    <dd>
+                        Razer DeathAdder V4 Pro
+                    </dd>
+                </div>
+                <div class="hardware-item">
+                    <dt>
+                        鍵盤
+                    </dt>
+                    <dd>
+                        ROG Strix Scope NX TKL Moonlight White
+                    </dd>
+                </div>
+                <div class="hardware-item">
+                    <dt>
+                        繪圖板
+                    </dt>
+                    <dd>
+                        Wacom CTL-4100
+                    </dd>
+                </div>
+                <div class="hardware-item">
+                    <dt>
+                        螢幕
+                    </dt>
+                    <dd>
+                        Acer VG250Q F 300hz
+                    </dd>
+                </div>
+                <div class="hardware-item">
+                    <dt>
+                        麥克風
+                    </dt>
+                    <dd>
+                        AVerMedia AM310 USB Microphone
+                    </dd>
+                </div>
+                <div class="hardware-item">
+                    <dt>
+                        攝影機
+                    </dt>
+                    <dd>
+                        Streamplify CAM
+                    </dd>
+                </div>
+            </dl>
         </section>
     </div>
 @endsection
